f0f861c3a22ca907fb0d559f8c3102d35563d19f
tricky <admin@qqfirst.com>
2023-05-31 13:35:28 +0800

    1:修改首页提示词 2：底部提示词 3：插件去掉google

--- components/Chat/Chat.tsx
+++ components/Chat/Chat.tsx
@@ -352,18 +352,16 @@ export const Chat = memo(({ stopConversationRef }: Props) => {
       {!(apiKey || serverSideApiKeyIsSet) ? (
         <div className="mx-auto flex h-full w-[300px] flex-col justify-center space-y-6 sm:w-[600px]">
           <div className="text-center text-4xl font-bold text-black dark:text-white">
-            Welcome to Chatbot UI
+            Welcome to ChatGPT
           </div>
           <div className="text-center text-lg text-black dark:text-white">
-            <div className="mb-8">{`Chatbot UI is an open source clone of OpenAI's ChatGPT UI.`}</div>
+            <div className="mb-8"></div>
             <div className="mb-2 font-bold">
-              Important: Chatbot UI is 100% unaffiliated with OpenAI.
             </div>
           </div>
           <div className="text-center text-gray-500 dark:text-gray-400">
             <div className="mb-2">
-              Chatbot UI allows you to plug in your API key to use this UI with
-              their API.
+
             </div>
             <div className="mb-2">
               It is <span className="italic">only</span> used to communicate
@@ -377,12 +375,12 @@ export const Chat = memo(({ stopConversationRef }: Props) => {
             <div>
               {t("If you don't have an OpenAI API key, you can get one here: ")}
               <a
-                href="https://platform.openai.com/account/api-keys"
+                href="javascript:void(0)"
                 target="_blank"
                 rel="noreferrer"
                 className="text-blue-500 hover:underline"
               >
-                openai.com
+                寻找客服索要
               </a>
             </div>
           </div>
